e93bdad658d4b0ebd2edef962c824820cf6e75b3
Shohel <nushohel2@gmail.com>
2021-03-22 16:27:19 +0600

    first commit

---
+++ src/FakeData/FakeVehicle.js
@@ -0,0 +1,88 @@
+const fakeVehicle=[{
+    "vehicleType": "Car",
+    "image": "https://i.ibb.co/C9NcPKP/Frame-2.png",
+    "passenger": '2',
+    "cost":47,
+    "key": 1
+    
+  }, {
+    "vehicleType": "Car",
+    "image": "https://i.ibb.co/C9NcPKP/Frame-2.png",
+    "passenger": '4',
+    "cost":47,
+    "key": 2
+  }, {
+    "vehicleType": "Car",
+    "image": "https://i.ibb.co/C9NcPKP/Frame-2.png",
+    "passenger": '50',
+    "cost":47,
+    "key": 3
+  }, {
+    "vehicleType": "Car",
+    "image": "https://i.ibb.co/C9NcPKP/Frame-2.png",
+    "passenger": "500",
+    "cost":47,
+    "key": 4
+  }
+  ,{
+    "vehicleType": "Bus",
+    "image": "https://i.ibb.co/X76J6Ps/Frame-1.png",
+    "passenger": "1",
+    "cost":47,
+    "key": 5
+  }, {
+    "vehicleType": "Bus",
+    "image": "https://i.ibb.co/X76J6Ps/Frame-1.png",
+    "passenger": "2",
+    "cost":47,
+    "key": 6
+  }, {
+    "vehicleType": "Bus",
+    "image": "https://i.ibb.co/X76J6Ps/Frame-1.png",
+    "passenger": "3",
+    "cost":47,
+    "key": 7
+  }, {
+    "vehicleType": "Bus",
+    "image": "https://i.ibb.co/X76J6Ps/Frame-1.png",
+    "passenger": "4",
+    "cost":47,
+    "key": 8
+  },{
+    "vehicleType": "Train",
+    "image": "https://i.ibb.co/Z2bb4qY/Group.png",
+    "passenger": "1",
+    "cost":47,
+    "key": 9
+  }, {
+    "vehicleType": "Train",
+    "image": "https://i.ibb.co/Z2bb4qY/Group.png",
+    "passenger": "2",
+    "cost":47,
+    "key": 10
+  }, {
+    "vehicleType": "Train",
+    "image": "https://i.ibb.co/Z2bb4qY/Group.png",
+    "passenger": "3",
+    "cost":47,
+    "key": 11
+  }, {
+    "vehicleType": "Bike",
+    "image": "https://i.ibb.co/WGMCSqq/Frame.png",
+    "passenger": "4",
+    "cost":47,
+    "key": 12
+  },{
+    "vehicleType": "Bike",
+    "image": "https://i.ibb.co/WGMCSqq/Frame.png",
+    "passenger": "1",
+    "cost":47,
+    "key": 13
+  }, {
+    "vehicleType": "Bike",
+    "image": "https://i.ibb.co/WGMCSqq/Frame.png",
+    "passenger": "2",
+    "cost":47,
+    "key": 14
+  }]
+  export default fakeVehicle;
